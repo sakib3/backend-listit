bf478c1f5cb0b656d1a2ee71ef5fcdfb0f0f4ada
unknown <sakib3@gmail.com>
2016-04-08 04:03:33 +0200

    ge_the_employee endpoint is added

--- app.py
+++ app.py
@@ -25,18 +25,30 @@ connection = PyMongo(app, config_prefix='MONGO')
 def toJson(data):
     return json.dumps(data, default=json_util.default)
 
-
+#get all employees
 @app.route('/employees/', methods=['GET'])
 def employees():
   if request.method == 'GET':
+
     lim = int(request.args.get('limit', 10))
     off = int(request.args.get('offset', 0))
     results = connection.db.employee.find().skip(off).limit(lim)
     json_results = []
+
     for result in results:
       json_results.append(result)
     return toJson(json_results)
 
+#get the employee
+@app.route('/employees/<ObjectId:employee_id>', methods=['GET'])
+def get_the_employee(employee_id):
+  if request.method == 'GET':
+
+    result = connection.db.employee.find_one_or_404(employee_id)
+    return toJson(result)
+
+
+#home
 @app.route('/', methods=['GET'])
 def home():
   return 'hello world'
