b5497f2a9cb44134d84cb52bfdc56748b0a52b41
unknown <sakib3@gmail.com>
2016-04-08 03:14:27 +0200

    init

---
+++ app.py
@@ -0,0 +1,48 @@
+import os, json
+from flask import Flask, request
+from bson import json_util
+from bson.objectid import ObjectId
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] if 'SECRET_KEY' in os.environ else 'this_should_be_configured'
+
+
+#define MongoDB server configuration
+app.config['MONGO_HOST'] = os.environ['MONGO_HOST'] if 'MONGO_HOST' in os.environ else 'localhost'
+app.config['MONGO_PORT'] = os.environ['MONGO_PORT'] if 'MONGO_PORT' in os.environ else 27017
+app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME'] if 'MONGO_DBNAME' in os.environ else 'listit'
+app.config['MONGO_USERNAME'] = os.environ['MONGO_USERNAME'] if 'MONGO_USERNAME' in os.environ else ''
+app.config['MONGO_PASSWORD'] = os.environ['MONGO_PASSWORD'] if 'MONGO_PASSWORD' in os.environ else ''
+
+
+# connect to MongoDB server
+connection = PyMongo(app, config_prefix='MONGO')
+
+
+#Convert Mongo object(s) to JSON
+def toJson(data):
+    return json.dumps(data, default=json_util.default)
+
+
+@app.route('/employees/', methods=['GET'])
+def employees():
+  if request.method == 'GET':
+    lim = int(request.args.get('limit', 10))
+    off = int(request.args.get('offset', 0))
+    results = connection.db.employee.find().skip(off).limit(lim)
+    json_results = []
+    for result in results:
+      json_results.append(result)
+    return toJson(json_results)
+
+@app.route('/', methods=['GET'])
+def home():
+  return 'hello world'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
